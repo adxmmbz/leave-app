a42a1fb729f33993f2d43cde705956e0a9c497db
Selva kumar <selva3vsk@outlook.com>
2025-08-09 13:09:09 +0530

    LoginApiProject

---
+++ LoginApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LoginApi.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LoginApi.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ LoginApi/Model/User.cs
@@ -0,0 +1,18 @@
+﻿namespace LoginApi.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+    }
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+    }
+    public class LoginResponse
+    {
+        public string Username { get; set; }
+        public string Role { get; set; }
+    }
+}
